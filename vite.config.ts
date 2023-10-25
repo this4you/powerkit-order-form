3d21e6248b3a09646156367623688684b84dca4d
vladmertvychenko <vladyslav.mertvychenko@y-tree.com>
2023-10-25 22:57:40 +0300

    Update bundle name logic

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/powerkit-order-form/',
+  build: {
+    rollupOptions: {
+      output: {
+        dir: 'dist',
+        entryFileNames: 'orderForm.js',
+        assetFileNames: 'orderForm.css',
+        chunkFileNames: "orderFormChunk.js",
+        manualChunks: undefined,
+      }
+    }
+  },
   plugins: [react()],
 })
